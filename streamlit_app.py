84f2196771d5efe1438e29dafc17f84176d8b106
IanAugusto888 <100525685+IanAugusto888@users.noreply.github.com>
2024-01-06 12:47:26 +0000

    Alteração da forma de seleção da lista de coleções - de MultSelect para SelectBox

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
+
+"""Validar os acessos as servidor"""
 import os
 # supabase import create_client, Client
 #secretsurl = 'https://jdwvqrblltbbffnivqcn.supabase.co'
@@ -13,21 +15,20 @@ import os
 #)
 
 #Colecoes_list = supabase.table('dColecoes').select('id', 'nome_USA').execute()
+""" Incluir uma validação de dados para selecionar a coleção """
 
-Colecao_selecionada = st.multiselect(
-    "Coleção", 
-    options=[" teste", "sera", "Que", "vai?"],
-    max_selections= 1,
+Colecao_selecionada = st.selectbox(
+    label= "Coleção:", 
+    options=["teste", "sera", "Que", "vai?"],
+    index=None,
     disabled=False,
-    placeholder= "Selecione a coleção desejada", 
-    )
+    # key="ID" -> No futuro, recebrá a ID da coleção.
+    help=" Clique e selecione a coleção que será utilizada na simulação de contratos",
+    placeholder= "Selecione a coleção desejada",
+    label_visibility= "visible"
+)
 
 """
-
-Validar os acessos as servidor
-
-Incluir uma validação de dados para selecionar a coleção
-
 Com base na seleção informada:
 - Exibir uma tabela com a distribuição de skins por nivel
 
