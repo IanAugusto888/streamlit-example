3d88a87dd8aa9b218323482c74db3e9082e10152
IanAugusto888 <100525685+IanAugusto888@users.noreply.github.com>
2024-01-06 10:22:21 +0000

    Versão inicial do projeto

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,31 @@ import pandas as pd
 import streamlit as st
 
 """
-Parabéns Ian, você Conseguiu, este é seu aplicativo ONLINE!
+
+Validar os acessos as servidor
+
+Incluir uma validação de dados para selecionar a coleção
+
+Com base na seleção informada:
+- Exibir uma tabela com a distribuição de skins por nivel
+
+Com base na coleção e nível selecionados:
+- Relacionar as skins do nível selecionado e do próximo (em caixas diferentes)
+
+- Incluir botão para solicitar a simulação dos contratos
+
+Quando o botão for clicado:
+- Solicitar do meu BD as informações de qualidade para Simulação do contrato.
+- Solicitar à Steam as informações de compra do nível atual e de venda do próximo nível.
+
+Após receber as informações solicitadas:
+- Criar lista de combinações
+- Calcular os resultados
+
+Exibir tabela com os resultado de cada contrato calculado
+
+Ao clicar no contrato calculado:
+- Exibir os itens que precisam ser comprados (com os links)
+- Exibir as possibilidades e os itens que podem retornar
+
 """
