9f944fac7c94ea2518b7198e1650b177a5eabfd7
IanAugusto888 <100525685+IanAugusto888@users.noreply.github.com>
2024-01-21 06:36:12 +0000

    Atualizações

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,70 +15,83 @@ supabase = create_client(
     secretskey 
 )
 
+Options_Colecoes = []
+
+
 Colecoes_df = pd.DataFrame(
     supabase.table('dColecoes'
     ).select('id', 'nome_USA'
     ).execute(
     ).data
 )
+with st.sidebar:
+    st.selectbox(
+        # Lista Coleção Selecionada
+        label = "Coleção:", 
+        options = Colecoes_df['nome_USA'],
+        index = None,
+        disabled = False,
+        key = "Colecao_selecionada",
+        help = "Clique e selecione a coleção que será utilizada na simulação de contratos",
+        placeholder = "Selecione a coleção desejada",
+        label_visibility = "visible"
+    )
 
-st.selectbox(
-    # Lista Coleção Selecionada
-    label = "Coleção:", 
-    options = Colecoes_df['nome_USA'], #["teste", "sera", "Que", "vai?"],
-    index = None,
-    disabled = False,
-    key = "Colecao_selecionada",
-    help = "Clique e selecione a coleção que será utilizada na simulação de contratos",
-    placeholder = "Selecione a coleção desejada",
-    label_visibility = "visible"
-)
 
-Id_Colecao_selecionada = Colecoes_df.loc[
-    (Colecoes_df['nome_USA'] == st.session_state.get('Colecao_selecionada'))
-    ]['id'].to_string(index=False)
+    if st.session_state.get('Colecao_selecionada') == None:
+        Id_Colecao_selecionada = None
 
-dArmas = supabase.table('dArmas'
-    ).select('nome, dArmas_Tipo(arma), dNivel(nivel,nivel_descricao_USA)'
-    ).eq('id_colecao', 1).execute().data #Mudar o int 1 para Id_Colecção_Selecionada Quando tiver um visual
+    else:
+        Id_Colecao_selecionada = Colecoes_df.loc[(Colecoes_df['nome_USA'] == st.session_state.get('Colecao_selecionada'))
+        ]['id'].to_string(index=False)
 
-dArmas_list = []
-for skin in dArmas:
-    nome = skin['nome']
-    arma = skin['dArmas_Tipo']['arma']
-    nivel =  skin['dNivel']['nivel']
-    nivel_USA = skin['dNivel']['nivel_descricao_USA']
+        dArmas = supabase.table('dArmas'
+        ).select('nome, dArmas_Tipo(arma), dNivel(nivel,nivel_descricao_USA)'
+        ).eq('id_colecao', Id_Colecao_selecionada).execute().data 
 
-    dArmas_list.append([nome, arma, nivel, nivel_USA])
+        opcoes_niveis = []
+        dArmas_list = []
+        for skin in dArmas:
+            nome = skin['nome']
+            arma = skin['dArmas_Tipo']['arma']
+            nivel =  skin['dNivel']['nivel']
+            nivel_USA = skin['dNivel']['nivel_descricao_USA']
 
-dArmas_df = pd.DataFrame(dArmas_list, columns = ['Nome', 'Arma', 'Nivel','Nivel USA'])
+            opcoes_niveis.append(nivel_USA)
+            dArmas_list.append([nome, arma, nivel, nivel_USA])
 
-opcoes_niveis = [
-    'Consumer Grade', 'Industrial Grade', 'Mil-Spec', 'Restricted','Classified', 'Covert', 'Contraband'
-    ]
+        dArmas_df = pd.DataFrame(dArmas_list, columns = ['Nome', 'Arma', 'Nivel','Nivel USA'])
+        opcoes_niveis = list(dict.fromkeys(opcoes_niveis))
 
-opcoes_niveis_nao_disponivel = [
-    'Contraband'
-    ]
+        st.selectbox(
+        "Niveis:",
+        options=opcoes_niveis[1:], 
+        index=None, 
+        key='Nivel_Selecionado', 
+        help='Clique e selecione o nível das skins que serão a base do contrato', 
+        disabled= False,
+        placeholder = "Selecione o nível desejado", 
+        label_visibility="visible")
 
-st.selectbox(
-    "Niveis:",
-    options=opcoes_niveis[:-1], 
-    index=None, 
-    key='Nivel_Selecionado', 
-    help='Clique e selecione o nível das skins que serão a base do contrato', 
-    disabled= False,
-    placeholder = "Selecione o nível desejado", 
-    label_visibility="visible")
+
+        if st.session_state.get('Nivel_Selecionado') != None:
+        
+            armas_base = dArmas_df.loc[(dArmas_df['Nivel USA'] == st.session_state.get('Nivel_Selecionado'))]
+            nivel_aramas_esperadas = int(dArmas_df.loc[(dArmas_df['Nivel USA'] == st.session_state.get('Nivel_Selecionado'))]['Nivel'].to_string(index=False)[0]) + 1
+
+            armas_esperadas = dArmas_df.loc[(dArmas_df['Nivel'] == nivel_aramas_esperadas)]
+
+            
+            
+        st.markdown("Armas base:")
+        st.dataframe(armas_base,  hide_index=True, use_container_width= True, column_order=("Arma","Nome"))
+    
+        st.markdown("Armas esperadas:")
+        st.dataframe(armas_esperadas,  hide_index=True, use_container_width= True, column_order=("Arma","Nome"))
+
 
 
 """
-Com base na seleção informada:
-- Exibir uma tabela com a distribuição de skins por nivel
-
-Com base na coleção e nível selecionados:
-- Relacionar as skins do nível selecionado e do próximo (em caixas diferentes)
-
 - Incluir botão para solicitar a simulação dos contratos
 
 Quando o botão for clicado:
