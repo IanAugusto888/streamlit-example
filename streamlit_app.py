32853a011af55d91c554acd1d5820f68b4e19c49
IanAugusto888 <100525685+IanAugusto888@users.noreply.github.com>
2024-01-22 00:21:47 +0000

    Inclusão integral da barra lateral com lógica de filtros

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,6 @@ import streamlit as st
 import os
 from supabase import create_client, Client
 
-"""  < PENDENTE : MIGRAR A URL E A CHAVE PARA A ÁREA SECRETA >"""
-
 secretsurl = 'https://jdwvqrblltbbffnivqcn.supabase.co'
 secretskey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impkd3ZxcmJsbHRiYmZmbml2cWNuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTg1NzU3NDgsImV4cCI6MjAxNDE1MTc0OH0.wpPFFHOZ4QV6i8FwSvS6e17Sdn86D_H88UOVWccgxPs'
 
@@ -17,82 +15,136 @@ supabase = create_client(
 
 Options_Colecoes = []
 
+#@st.cache_data
+def Carregar_Colecoes():
+    Colecoes = pd.DataFrame(
+        supabase.table('dColecoes'
+        ).select('id', 'nome_USA'
+        ).execute(
+        ).data
+    )
+    return Colecoes
 
-Colecoes_df = pd.DataFrame(
-    supabase.table('dColecoes'
-    ).select('id', 'nome_USA'
-    ).execute(
-    ).data
-)
-with st.sidebar:
+def SelecaoColecao():
+    st.subheader("Coleção:",divider='grey')
+    Colecoes_df = Carregar_Colecoes()
+    opcoes_colecao = Colecoes_df['nome_USA'].tolist()
+    opcoes_colecao.reverse()
+
     st.selectbox(
-        # Lista Coleção Selecionada
-        label = "Coleção:", 
-        options = Colecoes_df['nome_USA'],
-        index = None,
-        disabled = False,
-        key = "Colecao_selecionada",
-        help = "Clique e selecione a coleção que será utilizada na simulação de contratos",
-        placeholder = "Selecione a coleção desejada",
-        label_visibility = "visible"
+            # Lista Coleção Selecionada
+            label = "Coleção:", 
+            options = opcoes_colecao,
+            index = None,
+            disabled = False,
+            key = "Colecao_selecionada",
+            help = "Clique e selecione a coleção que será utilizada na simulação de contratos",
+            placeholder = "Selecione a coleção desejada",
+            label_visibility = "collapsed"
+        )
+
+#@st.cache_data
+def Carregar_armas_Colecao(NomeColecao):
+    dArmas = supabase.table('dArmas'
+        ).select('nome, dArmas_Tipo(arma), dNivel(nivel,nivel_descricao_USA), dColecoes()'
+        ).eq('dColecoes.nome_USA', NomeColecao ).execute().data 
+
+    dArmas_list = []
+    for skin in dArmas:
+        nome = skin['nome']
+        arma = skin['dArmas_Tipo']['arma']
+        nivel =  skin['dNivel']['nivel']
+        nivel_USA = skin['dNivel']['nivel_descricao_USA']
+
+        dArmas_list.append([nome, arma, nivel, nivel_USA])
+        dArmas_df = pd.DataFrame(dArmas_list, columns = ['Nome', 'Arma', 'Nivel','Nivel USA'])
+
+    return dArmas_df
+
+def SelecaoNivel(OpcoesNiveis):
+    st.radio(
+        "label",
+        OpcoesNiveis, 
+        index=0,  
+        key='Nivel_selecionado', 
+        disabled=False, 
+        horizontal=False, 
+        label_visibility='collapsed')
+
+def ExibirArmas(tabela):
+    st.subheader("Skins base",divider='grey')
+    st.dataframe(
+        tabela,  
+        hide_index=True, 
+        use_container_width= True, 
+        column_order=("Arma","Nome")
     )
 
+def Btn_ProcessarContratos():
+    st.button(
+        'Iniciar Simulação', 
+        key='btn_processarcontratos', 
+        help='Clique para iniciar a simulação dos contratoes', 
+        on_click=None, 
+        type="primary", 
+        disabled=False,
+        use_container_width=True
+    )
 
-    if st.session_state.get('Colecao_selecionada') == None:
-        Id_Colecao_selecionada = None
+def SideBar():
+    with st.sidebar:
 
-    else:
-        Id_Colecao_selecionada = Colecoes_df.loc[(Colecoes_df['nome_USA'] == st.session_state.get('Colecao_selecionada'))
-        ]['id'].to_string(index=False)
+        st.subheader("Contratos CS")
+        SelecaoColecao()
 
-        dArmas = supabase.table('dArmas'
-        ).select('nome, dArmas_Tipo(arma), dNivel(nivel,nivel_descricao_USA)'
-        ).eq('id_colecao', Id_Colecao_selecionada).execute().data 
+        if st.session_state.get('Colecao_selecionada') != None:
 
-        opcoes_niveis = []
-        dArmas_list = []
-        for skin in dArmas:
-            nome = skin['nome']
-            arma = skin['dArmas_Tipo']['arma']
-            nivel =  skin['dNivel']['nivel']
-            nivel_USA = skin['dNivel']['nivel_descricao_USA']
+            armascolecao = Carregar_armas_Colecao(st.session_state.get('Colecao_selecionada'))
+            opcoes_niveis = list(dict.fromkeys(armascolecao['Nivel USA'].tolist()))
+            opcoes_niveis.reverse()
 
-            opcoes_niveis.append(nivel_USA)
-            dArmas_list.append([nome, arma, nivel, nivel_USA])
+            SelecaoNivel(opcoes_niveis[:-1])
 
-        dArmas_df = pd.DataFrame(dArmas_list, columns = ['Nome', 'Arma', 'Nivel','Nivel USA'])
-        opcoes_niveis = list(dict.fromkeys(opcoes_niveis))
+            armasbase = armascolecao.loc[(armascolecao['Nivel USA'] == st.session_state.get('Nivel_selecionado'))]                     
+            armasesperadas = armascolecao.loc[(armascolecao['Nivel USA'] == opcoes_niveis[int(opcoes_niveis.index(st.session_state.get('Nivel_selecionado'))) + 1 ])]
 
-        st.selectbox(
-        "Niveis:",
-        options=opcoes_niveis[1:], 
-        index=None, 
-        key='Nivel_Selecionado', 
-        help='Clique e selecione o nível das skins que serão a base do contrato', 
-        disabled= False,
-        placeholder = "Selecione o nível desejado", 
-        label_visibility="visible")
+            ExibirArmas(armasbase)
+            ExibirArmas(armasesperadas)
+            Btn_ProcessarContratos()
+SideBar()
 
 
-        if st.session_state.get('Nivel_Selecionado') != None:
+      #  st.selectbox(
+       # "Niveis:",
+       # options=opcoes_niveis[1:], 
+       # index=None, 
+       # key='Nivel_Selecionado', 
+       # help='Clique e selecione o nível das skins que serão a base do contrato', 
+       # disabled= False,
+       # placeholder = "Selecione o nível desejado", 
+       # label_visibility="visible")
+
+
+        #if st.session_state.get('Nivel_Selecionado') != None:
         
-            armas_base = dArmas_df.loc[(dArmas_df['Nivel USA'] == st.session_state.get('Nivel_Selecionado'))]
-            nivel_aramas_esperadas = int(dArmas_df.loc[(dArmas_df['Nivel USA'] == st.session_state.get('Nivel_Selecionado'))]['Nivel'].to_string(index=False)[0]) + 1
+         #armas_base = dArmas_df.loc[(dArmas_df['Nivel USA'] == st.session_state.get('Nivel_Selecionado'))]
+          #  nivel_aramas_esperadas = int(dArmas_df.loc[(dArmas_df['Nivel USA'] == st.session_state.get('Nivel_Selecionado'))]['Nivel'].to_string(index=False)[0]) + 1
 
-            armas_esperadas = dArmas_df.loc[(dArmas_df['Nivel'] == nivel_aramas_esperadas)]
+           # armas_esperadas = dArmas_df.loc[(dArmas_df['Nivel'] == nivel_aramas_esperadas)]
 
             
             
-        st.markdown("Armas base:")
-        st.dataframe(armas_base,  hide_index=True, use_container_width= True, column_order=("Arma","Nome"))
+        #st.markdown("Armas base:")
+        #st.dataframe(armas_base,  hide_index=True, use_container_width= True, column_order=("Arma","Nome"))
     
-        st.markdown("Armas esperadas:")
-        st.dataframe(armas_esperadas,  hide_index=True, use_container_width= True, column_order=("Arma","Nome"))
+       # st.markdown("Armas esperadas:")
+        #st.dataframe(armas_esperadas,  hide_index=True, use_container_width= True, column_order=("Arma","Nome"))
 
 
 
 """
 - Incluir botão para solicitar a simulação dos contratos
+
 
 Quando o botão for clicado:
 - Solicitar do meu BD as informações de qualidade para Simulação do contrato.
@@ -108,4 +160,5 @@ Ao clicar no contrato calculado:
 - Exibir os itens que precisam ser comprados (com os links)
 - Exibir as possibilidades e os itens que podem retornar
 
+# < PENDENTE : MIGRAR A URL E A CHAVE PARA A ÁREA SECRETA >
 """
